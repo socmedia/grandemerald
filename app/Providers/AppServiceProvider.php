1333181b1408b1b354d92d467264f7c4383ca539
indraranuh-dev <indraranuh1@gmail.com>
2021-11-26 11:04:06 +0700

    Correct bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Modules\Contact\Entities\ContactsAttribute;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,16 +26,17 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        $contact = ContactsAttribute::orderBy('id')->get()->toArray();
+        // $contact = ContactsAttribute::orderBy('id')->get()->toArray();
 
+        $contact = [];
         $instagram = count($contact) > 0 ? $contact[0] : null;
         $facebook = count($contact) > 1 ? $contact[1] : null;
         $email = count($contact) > 2 ? $contact[2] : null;
         $whatsapp = count($contact) > 3 ? $contact[3] : null;
         $catalogue = count($contact) > 4 ? $contact[4] : null;
 
-        view()->share('instagram', $instagram['value']);
-        view()->share('facebook', $facebook['value']);
+        view()->share('instagram', $instagram ? $instagram['value'] : null);
+        view()->share('facebook', $facebook ? $facebook['value'] : null);
         view()->share('email', $email ? $email['value'] : null);
         view()->share('whatsapp', $whatsapp ? $whatsapp['value'] : null);
         view()->share('catalogue', $catalogue ? $catalogue['value'] : null);
